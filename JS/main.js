4763ab62fe29062f0057f4ee9f2dde639e81202d
Everton Cabral <evertoncab@gmail.com>
2020-12-21 22:55:23 -0300

    testando alguns métodos JS

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,29 @@
 var nome = 'Everton';
 var idade = 31;
 
+alert(`Bem vindo ${nome} você tem : ${idade} anos`);
 
-alert('Bem vindo '  + nome);
+var lista = ['pêssego', 'laranja', 'uva', 'goiaba'];
+lista.push('melão amarelo'); // insere um determinado dado na última posição do array
+console.log(lista)
+
+console.log(lista.reverse()) // imprime na tela a ordem inversa dos elementos do array
+lista.pop()  // retira o último elemento do array
+
+console.log(lista.toString()) // converte  todo o array para string com uso de vírgula
+console.log(lista.join(' | ')) //  converte  todo o array para string e utiliza o separador | 
+
+//trabalhando com dicionário
+var fruta = { nome: 'maçã', cor: 'vermelha' }
+console.log(fruta)
+console.log(fruta.nome)
+console.log(fruta.cor)
+
+// lista de dicionários
+
+var frutas = [{ nome: 'maçã', cor: 'vermelho' } , { nome: 'uva', cor: 'roxo' }, { nome: 'limão', cor: 'verde' }]
+console.log(frutas)
+console.log(frutas[0].nome)
+console.log(frutas[0].cor)
+
+
